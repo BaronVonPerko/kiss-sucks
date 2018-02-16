e24a5cbd97a4f8d0303106a8c2877d905b60ac16
Chris Perko <christopher.j.perko@windstream.com>
2018-02-16 15:59:30 -0600

    Enabling search on production

--- resources/views/_search.blade.php
+++ resources/views/_search.blade.php
@@ -1,7 +1,4 @@
-<div style="display:none">
-    <h3 class="section-title">Search</h3>
-    <small class="section-subtitle">Search for a song by the title or artist to see how often it has played</small>
+<h3 class="section-title">Search</h3>
+<small class="section-subtitle">Search for a song by the title or artist to see how often it has played</small>
 
-    <song-search></song-search>
-
-</div>
+<song-search></song-search>
