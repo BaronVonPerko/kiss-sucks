97a1bb16cbce598c944cfde787178f0cbb3d8f7a
Chris Perko <christopher.j.perko@windstream.com>
2018-02-16 15:54:30 -0600

    Hiding search for testing with production data

--- resources/views/_search.blade.php
+++ resources/views/_search.blade.php
@@ -1,5 +1,7 @@
-<h3 class="section-title">Search</h3>
-<small class="section-subtitle">Search for a song by the title or artist to see how often it has played</small>
+<div style="display:none">
+    <h3 class="section-title">Search</h3>
+    <small class="section-subtitle">Search for a song by the title or artist to see how often it has played</small>
 
-<song-search></song-search>
+    <song-search></song-search>
 
+</div>
